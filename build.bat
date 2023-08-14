9a3f39091de4434397b210315312f3195956aa3d
msaltnet <salt.jeong@gmail.com>
2023-08-14 22:38:40 +0900

    add build script and tool

---
+++ build.bat
@@ -0,0 +1,12 @@
+.\claat-windows-amd64.exe export .\_article\01_intro\01_intro.md
+.\claat-windows-amd64.exe export .\_article\02_semiconductor\02_semiconductor.md
+.\claat-windows-amd64.exe export .\_article\03_LED\03_LED.md
+.\claat-windows-amd64.exe export .\_article\04_ultrasound\04_ultrasound.md
+.\claat-windows-amd64.exe export .\_article\05_car_assembly\05_car_assembly.md
+.\claat-windows-amd64.exe export .\_article\06_car_circuit\06_car_circuit.md
+.\claat-windows-amd64.exe export .\_article\07_car_programming\07_car_programming.md
+.\claat-windows-amd64.exe export .\_article\91_LED_quiz\91_LED_quiz.md
+.\claat-windows-amd64.exe export .\_article\92_ultrasound_quiz\92_ultrasound_quiz.md
+.\claat-windows-amd64.exe export .\_article\93_buzzer_quiz\93_buzzer_quiz.md
+.\claat-windows-amd64.exe export .\_article\94_bluetooth_control\94_bluetooth_control.md
+.\claat-windows-amd64.exe export .\_article\95_smart_car\95_smart_car.md
